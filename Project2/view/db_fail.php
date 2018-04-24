303b2fc4ec7046644326d778ff74c61100b49b65
me252 <35499370+me252@users.noreply.github.com>
2018-04-23 21:33:46 -0400

    Add files via upload

---
+++ Project2/view/db_fail.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href ="..\style.css"
+rel="stylesheet">
+<title>DATBASE ERROR!!!!!</title>
+</head>
+<body>
+<h3>You have encountered a Database Error: Please contact Database Admin.</h3>
+<br><br><br>
+<a href="..\index.php">Click Here to try logging in again!</a>
+</body>
+</html>
